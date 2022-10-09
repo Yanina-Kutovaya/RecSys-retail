7a30fe4f5fc5de0033da8aedcea67c5b28299d1f
Yanina Kutovaya <kutovaiayp@yandex.ru>
2022-10-10 01:39:08 +0300

    Fix make_dataset.py

--- src/recsys_retail/data/make_dataset.py
+++ src/recsys_retail/data/make_dataset.py
@@ -18,17 +18,20 @@ def load_data(
 
     if data_path is None:
         data_path = TRAIN_URL
+    logging.info(f'Reading dataset from {data_path}...')
     data = pd.read_csv(data_path)
 
     if item_path is None:
         item_path = ITEM_FEATURES_URL
-    item_features = pd.read_csv(ITEM_FEATURES_URL)
+    logging.info(f'Reading item_features from {item_path}...')
+    item_features = pd.read_csv(item_path)
     item_features.columns = map(str.lower, item_features.columns)
     item_features.rename(columns={'product_id': 'item_id'}, inplace=True)
 
     if user_path is None:
         user_path = USER_FEATURES_URL
-    user_features = pd.read_csv(USER_FEATURES_URL)
+    logging.info(f'Reading user_features from {user_path}...')
+    user_features = pd.read_csv(user_path)
     user_features.columns = map(str.lower, user_features.columns)
     user_features.rename(columns={'household_key': 'user_id'}, inplace=True)
 
