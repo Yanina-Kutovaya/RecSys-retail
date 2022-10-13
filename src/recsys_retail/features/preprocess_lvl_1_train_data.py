a6855357015ec087916c76c5259b80e068b8f6a0
Yanina Kutovaya <kutovaiayp@yandex.ru>
2022-10-13 08:51:44 +0300

    Add comments to preprocess_lvl_1_train_data.py

--- src/recsys_retail/features/preprocess_lvl_1_train_data.py
+++ src/recsys_retail/features/preprocess_lvl_1_train_data.py
@@ -3,6 +3,7 @@ import logging
 import numpy as np
 import pandas as pd
 from pickle import dump, load
+from typing import Optional
 
 from src.recsys_retail.data.make_dataset import load_data
 from src.recsys_retail.features.prefilter import prefilter_items
@@ -15,27 +16,27 @@ logger = logging.getLogger(__name__)
 
 __all__ = ['preprocess_lvl_1_data']
 
-TRAIN_DATA_LEVEL_1_PATH = 'data_train_lvl_1.csv.zip'
+PATH = '/data/01_raw/'
+TRAIN_DATA_LEVEL_1_PATH = PATH + 'data_train_lvl_1.csv.zip'
 
+def get_lvl_1_train_dataset(
+    data_train_lvl_1: pd.DataFrame,
+    item_features: pd.DataFrame,
+    user_features:pd.DataFrame,
+    train_data_lvl_1_path: Optional[str] = None
+    ) -> pd.DataFrame:
 
-def get_lvl_1_train_dataset(train_data_lvl_1_path = None) -> pd.DataFrame:
-
+    """ 
+    Prepares dataset which combines transactions, user features 
+    and item features to be passed to user-item matrix for recommender.
+    """
+    
     logging.info('Preprocessing level 1 train dataset...')
-
-    data, item_features, user_features = load_data()
-
-    # For the 1st level we use older data leaving 9 weeks for validation:
-    # 6 weeks for the 1st level validation and 3 weeks for the 2nd level.
-    val_lvl_1_size_weeks = 6
-    val_lvl_2_size_weeks = 3
-    t0 = data['week_no'].max() - (val_lvl_1_size_weeks + val_lvl_2_size_weeks)
-    data_train_lvl_1 = data[data['week_no'] < t0]
 
     # Prefilter items
     data_train_lvl_1 = prefilter_items(data_train_lvl_1, item_features)
 
-    # Preprocess user features and merge with transactions data
-    os.chdir('../data/02_intermediate')
+    # Preprocess user features and merge with transactions data    
     user_features_transformed = transform_user_features(user_features)
     data_train_lvl_1 = pd.merge(
         data_train_lvl_1, user_features_transformed, on='user_id', how='left'
@@ -45,8 +46,7 @@ def get_lvl_1_train_dataset(train_data_lvl_1_path = None) -> pd.DataFrame:
     data_train_lvl_1 = pd.merge(
         data_train_lvl_1, item_features_transformed, on='item_id', how='left'
     )
-    # Save results
-    os.chdir('../03_primary')
+    # Save results    
     if train_data_lvl_1_path is None:
         train_data_lvl_1_path = TRAIN_DATA_LEVEL_1_PATH
     data_train_lvl_1.to_csv(train_data_lvl_1_path, compression='zip')
