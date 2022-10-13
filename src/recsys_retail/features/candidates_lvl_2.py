ce9ea1deb7c53ec4ebdd699153bdfe72f15cbd32
Yanina Kutovaya <kutovaiayp@yandex.ru>
2022-10-13 13:12:17 +0300

    Fix comments in candidates_lvl_2.py

--- src/recsys_retail/features/candidates_lvl_2.py
+++ src/recsys_retail/features/candidates_lvl_2.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 __all__ = ['getting_candidates_for_level_2']
 
 
-N_CANDIDATES = 300
+N_ITEMS = 5
 PATH = 'data/04_feature/'
 CANDIDATES_PATH = PATH + 'candidates_lvl_2.csv.zip'
 
@@ -18,13 +18,13 @@ def get_candidates(
     data_train_lvl_1: pd.DataFrame,
     data_val_lvl_1: pd.DataFrame, 
     data_val_lvl_2: pd.DataFrame, 
-    n_candidates: Optional[int] = None,
+    n_items: Optional[int] = None,
     candidates_path: Optional[str] = None
     ):
 
     """
-    Selects candidates for level 2 and long list of preferred items for each user.
-    Later the model will select short lists of items (top-5) from the long lists.        
+    Selects candidates for level 2 with n_items to be recommended
+    for each of the candidate (top-5).        
     """
     
     recommender = MainRecommender(data_train_lvl_1)
@@ -40,18 +40,18 @@ def get_candidates(
     current_users = list(set(users_lvl_2) & set(users_lvl_1))    
     new_users = list(set(users_lvl_2) - set(users_lvl_1))
 
-    if n_candidates is None:
-        n_candidates = N_CANDIDATES
+    if n_items is None:
+        n_items = N_ITEMS
 
     df = pd.DataFrame(users_lvl_2, columns=['user_id'])
     cond_1 = df['user_id'].isin(current_users)
     df.loc[cond_1, 'candidates'] = df.loc[cond_1, 'user_id'].apply(
-        lambda x: recommender.get_own_recommendations(x, n_candidates)
+        lambda x: recommender.get_own_recommendations(x, n_items)
     )
     if new_users:
         cond_2 = df['user_id'].isin(new_users)
         df.loc[cond_2, 'candidates'] = df.loc[cond_2, 'user_id'].apply(
-                lambda x: recommender.overall_top_purchases[:n_candidates]
+                lambda x: recommender.overall_top_purchases[:n_items]
         )
     if candidates_path is None:
         candidates_path = CANDIDATES_PATH
