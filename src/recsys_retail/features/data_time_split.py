4d4a4e591ba5161b6860de4a5a569178e5f7b328
Yanina Kutovaya <kutovaiayp@yandex.ru>
2022-10-13 09:16:07 +0300

    Fix data_time_split.py

--- src/recsys_retail/features/data_time_split.py
+++ src/recsys_retail/features/data_time_split.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 
 __all__ = ['splitting_data_2_levels']
 
-PATH = '01_raw/'
+PATH = 'data/01_raw/'
 TRAIN_DATA_LEVEL_1_PATH = PATH + 'data_train_lvl_1.csv.zip'
 VALID_DATA_LEVEL_1_PATH = PATH + 'data_val_lvl_1.csv.zip'
 VALID_DATA_LEVEL_2_PATH = PATH + 'data_val_lvl_2.csv.zip'
@@ -22,11 +22,11 @@ def time_split(
     valid_data_level_2_path: Optional[str] = None    
     ) -> pd.DataFrame:
     """ 
-    Train-validation-test time split for two-stage recommender system
-    
+    Train-validation-test time split for two-stage recommender system.
+
     Train - validation - test schema:
     -- old purchases -- | -- 6 weeks-- | -- 3 weeks--
-
+    
     For the 1st level we use older data leaving 9 weeks for validation:
     6 weeks for the 1st level validation and 3 weeks for the 2nd level.
     """
@@ -44,8 +44,7 @@ def time_split(
     data_val_lvl_1 = data[(data['week_no'] >= t0) & (data['week_no'] < t1)]
     data_val_lvl_2 = data[data['week_no'] >= t1]
 
-    if save_split:
-        os.chdir('../data')
+    if save_split:        
         if train_data_lvl_1_path is None:
             train_data_lvl_1_path = TRAIN_DATA_LEVEL_1_PATH
         data_train_lvl_1.to_csv(train_data_lvl_1_path, compression='zip')
