8f7c41be7fd7134492488e849233649a53a1a552
Yanina Kutovaya <kutovaiayp@yandex.ru>
2022-10-15 03:52:32 +0300

    Fix train.py

--- src/recsys_retail/models/train.py
+++ src/recsys_retail/models/train.py
@@ -1,6 +1,6 @@
 import logging
 import pandas as pd
-from typing import Optional
+from typing import Optional, Tuple
 
 from src.recsys_retail.data.make_dataset import load_data
 from src.recsys_retail.features.data_time_split import time_split
@@ -14,14 +14,15 @@ logger = logging.getLogger(__name__)
 __all__ = ['generate_lvl_2_dataset']
 
 N_ITEMS = 300
-ITEM_FEATURES_TRANSFORMED_PATH = 'data/02_intermediate/item_features_transformed.csv.zip'
-USER_FEATURES_TRANSFORMED_PATH = 'data/02_intermediate/user_features_transformed.csv.zip'
-USER_ITEM_FEATURES_PATH = 'data/04_feature/user_item_features.csv.zip'
+PATH_1 = 'data/02_intermediate/'
+ITEM_FEATURES_TRANSFORMED_PATH = PATH_1 + 'item_features_transformed.csv.zip'
+USER_FEATURES_TRANSFORMED_PATH = PATH_1 + 'user_features_transformed.csv.zip'
 
-PATH = 'data/05_model_input'
-DATASET_LVL_2_PATH = PATH + 'dataset_lvl_2.csv.zip'
+PATH_2 = 'data/05_model_input'
+TRAIN_DATASET_LVL_2_PATH = PATH_2 + 'dataset_lvl_2.csv.zip'
 
-def get_dataset_lvl_2(
+
+def get_train_dataset_lvl_2(
     data: pd.DataFrame, 
     item_features: pd.DataFrame, 
     user_features: pd.DataFrame,
@@ -29,7 +30,7 @@ def get_dataset_lvl_2(
     item_features_transformed_path: Optional[str] = None,
     user_features_transformed_path: Optional[str] = None,
     user_item_features_path: Optional[str] = None,
-    dataset_lvl_2_path: Optional[str] = None
+    train_dataset_lvl_2_path: Optional[str] = None
     ) -> pd.DataFrame:
 
     logging.info('Generating level 1 dataset...')
@@ -47,6 +48,28 @@ def get_dataset_lvl_2(
     logging.info('Generating new user-item features...')
     user_item_features = get_user_item_features(recommender, data_train_lvl_1)
 
+    item_features_transformed, user_features_transformed = load_transformed_features()   
+    
+    train_dataset_lvl_2 = get_targets_lvl_2(
+        users_lvl_2, data_train_lvl_2, item_features_transformed, 
+        user_features_transformed, user_item_features
+    )
+
+    logging.info('Saving level 2 dataset...')
+
+    if train_dataset_lvl_2_path is None:
+        train_dataset_lvl_2_path = TRAIN_DATASET_LVL_2_PATH
+    train_dataset_lvl_2.to_csv(
+        train_dataset_lvl_2_path, index=False, compression='zip'
+    )
+
+    return dataset_lvl_2
+
+def load_transformed_features(
+    item_features_transformed_path: Optional[str] = None,
+    user_features_transformed_path: Optional[str] = None,
+    ) -> Tuple[pd.DataFrame, pd.DataFrame]:
+
     logging.info(
         f'Reading item_features_transformed_path from {item_features_transformed_path}...'
     )
@@ -63,15 +86,4 @@ def get_dataset_lvl_2(
     user_features_transformed = pd.read_csv(
         user_features_transformed_path, compression='zip'
     )
-    dataset_lvl_2 = get_targets_lvl_2(
-        users_lvl_2, data_train_lvl_2, item_features_transformed, 
-        user_features_transformed, user_item_features
-    )
-
-    logging.info('Saving level 2 dataset...')
-
-    if dataset_lvl_2_path is None:
-        dataset_lvl_2_path = DATASET_LVL_2_PATH
-    targets_lvl_2.to_csv(dataset_lvl_2_path, index=False, compression='zip')
-
-    return dataset_lvl_2
+    return item_features_transformed, user_features_transformed
