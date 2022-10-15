92a643ee7763fb06bb85fc552cab21e07b82b6de
Yanina Kutovaya <kutovaiayp@yandex.ru>
2022-10-15 17:56:17 +0300

    Fix targets.py

--- src/recsys_retail/features/targets.py
+++ src/recsys_retail/features/targets.py
@@ -8,11 +8,7 @@ logger = logging.getLogger(__name__)
 
 __all__ = ['generate_targets']
 
-
 N_ITEMS = 300
-
-PATH = 'data/05_model_input/'
-TARGET_LVL_2_PATH = PATH + 'targets_lvl_2.csv.zip'
 
 
 def get_targets_lvl_2(
@@ -21,8 +17,7 @@ def get_targets_lvl_2(
     item_features_transformed: pd.DataFrame,  
     user_features_transformed: pd.DataFrame,
     user_item_features:pd.DataFrame,     
-    n_items: Optional[int] = None,
-    targets_lvl_2_path: Optional[str] = None 
+    n_items: Optional[int] = None 
     ) -> pd.DataFrame:
     """ 
     Generates targets for candidates from level 2 (data_train_lvl_2 6-week period,
@@ -53,9 +48,5 @@ def get_targets_lvl_2(
     targets_lvl_2 = targets_lvl_2.merge(
         user_item_features, on=['user_id', 'item_id'], how='left'
     ).drop_duplicates()
-
-    if targets_lvl_2_path is None:
-        targets_lvl_2_path = TARGET_LVL_2_PATH
-    targets_lvl_2.to_csv(targets_lvl_2_path, index=False, compression='zip')
-
+    
     return targets_lvl_2
