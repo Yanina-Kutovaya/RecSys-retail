56cda320f1373bdd7c17ec8e97c951501725da17
Yanina Kutovaya <kutovaiayp@yandex.ru>
2022-10-20 03:35:43 +0300

    Fix saving data_time_split.py

--- src/recsys_retail/features/data_time_split.py
+++ src/recsys_retail/features/data_time_split.py
@@ -8,23 +8,9 @@ logger = logging.getLogger(__name__)
 
 __all__ = ['splitting_data_2_levels']
 
-PATH = 'data/01_raw/'
-# 3-periods split
-TRAIN_DATA_LEVEL_1_PATH = PATH + 'data_train_lvl_1.csv.zip'
-VALID_DATA_LEVEL_1_PATH = PATH + 'data_val_lvl_1.csv.zip'
-VALID_DATA_LEVEL_2_PATH = PATH + 'data_val_lvl_2.csv.zip'
-
-# 2 periods split
-DATA_TRAIN_PATH = PATH + 'data_train.csv.zip'
-DATA_VALID_PATH = PATH + 'data_train.csv.zip'
-
 
 def time_split(
-    data: pd.DataFrame,
-    save_split = True,
-    train_data_lvl_1_path: Optional[str] = None,
-    valid_data_level_1_path: Optional[str] = None,
-    valid_data_level_2_path: Optional[str] = None    
+    data: pd.DataFrame
     ) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     
     """ 
@@ -48,38 +34,18 @@ def time_split(
 
     data_train_lvl_1 = data[data['week_no'] < t0]
     data_val_lvl_1 = data[(data['week_no'] >= t0) & (data['week_no'] < t1)]
-    data_val_lvl_2 = data[data['week_no'] >= t1]
-
-    if save_split:        
-        if train_data_lvl_1_path is None:
-            train_data_lvl_1_path = TRAIN_DATA_LEVEL_1_PATH
-        data_train_lvl_1.to_csv(
-            train_data_lvl_1_path, index=False, compression='zip'
-        )
-        if valid_data_level_1_path is None:
-            valid_data_level_1_path = VALID_DATA_LEVEL_1_PATH
-        data_val_lvl_1.to_csv(
-            valid_data_level_1_path, index=False, compression='zip'
-        )
-        if valid_data_level_2_path is None:
-            valid_data_level_2_path = VALID_DATA_LEVEL_2_PATH
-        data_val_lvl_2.to_csv(
-            valid_data_level_2_path, index=False, compression='zip'
-        )
+    data_val_lvl_2 = data[data['week_no'] >= t1]   
 
     return data_train_lvl_1, data_val_lvl_1, data_val_lvl_2
 
 
 def time_split_2(
-    data: pd.DataFrame,
-    save_split = True,
-    data_train_path: Optional[str] = None,
-    data_valid_path: Optional[str] = None,       
+    data: pd.DataFrame
     ) -> Tuple[pd.DataFrame, pd.DataFrame]:
     
     """ 
-    Train-validation time split for two-stage recommender system.
-    To be used for training model on full dataset (without time 
+    Train-validation time split for two-stage recommender system;
+    to be used for training model on full dataset (without time 
     allocation for test).
 
     Train - validation schema:
@@ -94,17 +60,5 @@ def time_split_2(
     validation_weeks = 6
     data_train = data[data['week_no'] < data['week_no'].max() - validation_weeks]
     data_valid = data[data['week_no'] >= data['week_no'].max() - validation_weeks]
-
-    if save_split:        
-        if data_train_path is None:
-            data_train_path = DATA_TRAIN_PATH
-        data_train.to_csv(
-            data_train_path, index=False, compression='zip'
-        )
-        if data_valid_path is None:
-            data_valid_path = DATA_VALID_PATH
-        data_valid.to_csv(
-            data_valid_path, index=False, compression='zip'
-        )       
 
     return data_train, data_valid
