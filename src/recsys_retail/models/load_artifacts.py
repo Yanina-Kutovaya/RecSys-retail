1fb0ada7aa428ac45cefc3a182fc14293dc107ef
Yanina Kutovaya <kutovaiayp@yandex.ru>
2022-10-31 14:44:53 +0300

    Fix path in load_artifacts.py

--- src/recsys_retail/models/load_artifacts.py
+++ src/recsys_retail/models/load_artifacts.py
@@ -4,13 +4,13 @@ import pandas as pd
 from typing import Optional, Tuple
 
 PATH = '../'
+RECOMMENDER_PATH = 'models/recommender_v1'
+USER_FEATURES_TRANSFORMED_PATH = 'data/03_primary/user_features_transformed.csv.zip'
+ITEM_FEATURES_TRANSFORMED_PATH = 'data/03_primary/item_features_transformed.csv.zip'
+USER_ITEM_FEATURES_PATH = 'data/04_feature/user_item_features.csv.zip'
 
-RECOMMENDER_PATH = PATH + 'models/recommender_v1'
-USER_FEATURES_TRANSFORMED_PATH = PATH + 'data/03_primary/user_features_transformed.csv.zip'
-ITEM_FEATURES_TRANSFORMED_PATH = PATH + 'data/03_primary/item_features_transformed.csv.zip'
-USER_ITEM_FEATURES_PATH = PATH + 'data/04_feature/user_item_features.csv.zip'
-
-def load_inference_artifacts(    
+def load_inference_artifacts(
+    path: Optional[str] = None,    
     recommender_path: Optional[str] = None,
     user_features_transformed_path: Optional[str] = None,
     item_features_transformed_path: Optional[str] = None,
@@ -20,21 +20,23 @@ def load_inference_artifacts(
     Loads recommender, user_features_transformed, item_features_transformed, 
     user_item_features for inference
     """
+    if path is None:
+        path = PATH
 
     if recommender_path is None:
-        recommender_path = RECOMMENDER_PATH
+        recommender_path = path + RECOMMENDER_PATH
     recommender = joblib.load(recommender_path)
 
     if user_features_transformed_path is None:
-        user_features_transformed_path = USER_FEATURES_TRANSFORMED_PATH
+        user_features_transformed_path = path + USER_FEATURES_TRANSFORMED_PATH
     user_features_transformed = pd.read_csv(user_features_transformed_path)
 
     if item_features_transformed_path is None:
-        item_features_transformed_path = ITEM_FEATURES_TRANSFORMED_PATH
+        item_features_transformed_path = path + ITEM_FEATURES_TRANSFORMED_PATH
     item_features_transformed = pd.read_csv(item_features_transformed_path)
 
     if user_item_features_path is None:
-        user_item_features_path = USER_ITEM_FEATURES_PATH
+        user_item_features_path = path + USER_ITEM_FEATURES_PATH
     user_item_features = pd.read_csv(user_item_features_path)
 
     return (recommender, user_features_transformed, 
