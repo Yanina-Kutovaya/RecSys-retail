fbd9a3c1cd5d1304dc73c16a9e9eb5286116ab17
Yanina Kutovaya <kutovaiayp@yandex.ru>
2022-10-31 22:14:16 +0300

    Add user list to main.py

--- service/main.py
+++ service/main.py
@@ -10,7 +10,9 @@ import numpy as np
 from typing import Optional
 
 from fastapi import FastAPI, HTTPException
+from fastapi.encoders import jsonable_encoder
 from starlette_exporter import PrometheusMiddleware, handle_metrics
+from pydantic import BaseModel
 
 from src.recsys_retail.models.serialize import load
 from src.recsys_retail.models.inference_tools import preprocess
@@ -27,6 +29,14 @@ MODEL = os.getenv("MODEL", default='LightGBM_v1')
 class Model:
     classifier = None
 
+
+class User(BaseModel):
+    user_id: int
+
+
+class Users(BaseModel):
+    user_ids: list
+    
  
 @app.on_event('startup')
 def load_model():
@@ -39,10 +49,11 @@ def read_healthcheck():
 
 
 @app.post('/predict')
-def predict(user_id: int):
+def predict(user_id: int, user: User):
     if Model.classifier is None:
         raise HTTPException(status_code=503, detail='No model loaded')
-    try:        
+    try:
+        user_id = jsonable_encoder(user)['user_id']       
         df = preprocess(user_id)
         predictions = Model.classifier.predict(df)
         results = get_recommendations(df, predictions)        
@@ -51,3 +62,19 @@ def predict(user_id: int):
         raise HTTPException(status_code=400, detail=str(e))
         
     return {'user_id': user_id, 'recommendations': recs}
+
+
+@app.post('/predict_user_list')
+def predict(user_ids: list, users: Users):
+    if Model.classifier is None:
+        raise HTTPException(status_code=503, detail='No model loaded')
+    try:
+        user_ids = jsonable_encoder(users)['user_ids']        
+        df = preprocess(user_ids, user_list=True)
+        predictions = Model.classifier.predict(df)
+        results = get_recommendations(df, predictions)        
+        recs = {results['user_id']: results['recommendations'].tolist()}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+        
+    return {'user_ids': user_ids, 'recommendations': recs}
