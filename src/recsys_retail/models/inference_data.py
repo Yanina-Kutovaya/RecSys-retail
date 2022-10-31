51789087cd7c7ae60b71b0cd1e22c10f9f873fde
Yanina Kutovaya <kutovaiayp@yandex.ru>
2022-10-31 16:24:23 +0300

    Revert "inference_data.py"
    This reverts commit 1335bdf65687464782f97bc2bb2465ff7db7b4c1.

--- src/recsys_retail/models/inference_data.py
+++ src/recsys_retail/models/inference_data.py
@@ -2,22 +2,11 @@ import pandas as pd
 import joblib
 from typing import Optional
 
+
+PATH = 'models/'
+RECOMMENDER_PATH = PATH + 'recommender_v1' 
+
 N_ITEMS = 100
-
-PATH_1 = 'models/'
-RECOMMENDER_PATH = PATH_1 + 'recommender_v1' 
-
-PATH_2 = 'data/01_raw/'
-DATA_TRAIN_PATH = PATH_2 + 'data_train.csv.zip'
-DATA_VALID_PATH = PATH_2 + 'data_valid.csv.zip'
-
-PATH_3 = 'data/02_intermediate/'
-USER_FEATURES_TRANSFORMED_PATH = PATH_3 + 'user_features_transformed.csv.zip'
-ITEM_FEATURES_TRANSFORMED_PATH = PATH_3 + 'item_features_transformed.csv.zip'
-
-PATH_4 = 'data/04_feature/' 
-USER_ITEM_FEATURES_FINAL_PATH = PATH_4 + 'user_item_features_final.csv.zip'
-
 
 def preprocess(
     test: pd.DataFrame,
@@ -62,35 +51,11 @@ def load_artefacts(
     item_features_transformed_path: Optional[str] = None,
     user_features_transformed_path: Optional[str] = None,
     user_item_features_final_path: Optional[str] = None,
-    ):
-
-    """
-    Loads artefacts for preprocess function
-    """
+):
 
     if recommender_path is None:
         recommender_path = RECOMMENDER_PATH
     recommender = joblib.load(recommender_path)
-
-    if data_train_path is None:
-        data_train_path = DATA_TRAIN_PATH
-    data_train = pd.read_csv(data_train_path)
-
-    if data_valid_path is None:
-        data_valid_path = DATA_VALID_PATH
-    data_valid = pd.read_csv(data_valid_path)
-
-    if item_features_transformed_path is None:
-        item_features_transformed_path = ITEM_FEATURES_TRANSFORMED_PATH
-    item_features_transformed = pd.read_csv(item_features_transformed_path)
-
-    if user_features_transformed_path is None:
-        user_features_transformed_path = USER_FEATURES_TRANSFORMED_PATH
-    user_features_transformed = pd.read_csv(user_features_transformed_path)
-
-    if user_item_features_final_path is None:
-        user_item_features_final_path = USER_ITEM_FEATURES_FINAL_PATH
-    user_item_features_final = pd.read_csv(user_item_features_final_path)
 
     return (
         recommender, 
