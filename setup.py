e06cb96589ff7b9026f0af6b60cb53f914975cb9
Yanina Kutovaya <kutovaiayp@yandex.ru>
2022-11-08 17:56:04 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='recsys-retail',
+      version='1.0',
+      description='RecSys retail recommender system',
+      author='Yanina Kutovaya',
+      author_email='kutovaiayp@yandex.ru',
+      url='https://github.com/Yanina-Kutovaya/RecSys-retail.git',
+      package_dir={"": "src"},
+      packages=find_packages(where="src"),
+     )
