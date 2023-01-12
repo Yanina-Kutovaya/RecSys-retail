f4ea11b84e4479f1211e3e73574d9ef90cbf0508
Yanina Kutovaya <kutovaiayp@yandex.ru>
2023-01-13 01:59:57 +0300

    Copy model to YC S3

--- src/recsys_retail/models/serialize.py
+++ src/recsys_retail/models/serialize.py
@@ -15,10 +15,14 @@ def store(model_cb, filename: str, path: str = "default"):
 
     logger.info(f"Dumpung model into {filepath}")
     model_cb.save_model(filepath)
-    
+
     session = boto3.session.Session()
-    s3 = session.client(service_name='s3', endpoint_url='https://storage.yandexcloud.net')
-    s3.upload_file(filepath, 'recsys-retail-model-registry', 'current_model/' + filename) 
+    s3 = session.client(
+        service_name="s3", endpoint_url="https://storage.yandexcloud.net"
+    )
+    s3.upload_file(
+        filepath, "recsys-retail-model-registry", "current_model/" + filename
+    )
 
 
 def load(filename: str, path: str = "default"):
