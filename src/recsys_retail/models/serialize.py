8c3d6c18d04d4d5d1b0f739493f8299add9f7b0c
Yanina Kutovaya <kutovaiayp@yandex.ru>
2023-01-18 01:14:27 +0300

    Fix joblib

--- src/recsys_retail/models/serialize.py
+++ src/recsys_retail/models/serialize.py
@@ -2,6 +2,7 @@ import os
 import logging
 import boto3
 import lightgbm as lgb
+import joblib
 
 __all__ = ["store", "load"]
 
@@ -11,10 +12,10 @@ logger = logging.getLogger()
 def store(model_lgb, filename: str, path: str = "default"):
     if path == "default":
         path = models_path()
-    filepath = os.path.join(path, filename + ".txt")
+    filepath = os.path.join(path, filename + ".joblib")
 
-    logger.info(f"Dumpung model into {filepath}")
-    model_lgb.save_model(filepath)
+    logger.info(f"Dumpung model into {filepath}")       
+    joblib.dump(model_lgb, filepath)
 
     session = boto3.session.Session()
     s3 = session.client(
@@ -28,11 +29,12 @@ def store(model_lgb, filename: str, path: str = "default"):
 def load(filename: str, path: str = "default"):
     if path == "default":
         path = models_path()
-    filepath = os.path.join(path, filename + ".txt")
+    filepath = os.path.join(path, filename + ".joblib")
 
-    logger.info(f"Loading model from {filepath}")    
+    logger.info(f"Loading model from {filepath}")
 
-    return lgb.Booster(model_file=filepath)
+    return joblib.load(filepath)
+    
 
 
 def models_path() -> str:
