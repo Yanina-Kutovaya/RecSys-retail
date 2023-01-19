f937292e2441f6fc7d7fe83a0a3c6b73972bcabd
Yanina Kutovaya <kutovaiayp@yandex.ru>
2023-01-19 19:06:09 +0300

    Add TfidfVectorizer to item_features.py

--- src/recsys_retail/features/item_features.py
+++ src/recsys_retail/features/item_features.py
@@ -3,8 +3,6 @@ import logging
 import numpy as np
 import pandas as pd
 import category_encoders as ce
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.pipeline import make_pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction import text
 from pickle import dump, load
@@ -15,6 +13,15 @@ logger = logging.getLogger(__name__)
 
 __all__ = ["transform_item_features"]
 
+
+FEATURES_FOR_COUNT_ENCODER = [
+    "manufacturer",
+    "department",
+    "commodity_desc",
+    "sub_commodity_desc",
+    "curr_size_of_product",
+]
+FEATURE_FOR_HASHING_ENCODER = "manufacturer"
 
 ADD_STOP_WORDS = [
     "10",
@@ -31,81 +38,92 @@ ADD_STOP_WORDS = [
 CONCAT_LIST = ["commodity_desc", "sub_commodity_desc"]
 
 
-def item_features_frequency_encoder(item_features: pd.DataFrame) -> pd.DataFrame:
+def fit_transform_item_features(
+    item_features: pd.DataFrame,
+    count_cols=None,
+    hashing_enc_col=None,
+) -> pd.DataFrame:
     """
-    Assumes all the features as categorical and replaces categories
-    with their frequencies.
+    Generates new item featurs for train dataset.
     """
+    logging.info("Transforming item_features for train dataset...")
 
-    logging.info("Encoding item features with frequencies...")
+    if count_cols is None:
+        count_cols = FEATURES_FOR_COUNT_ENCODER
+    if hashing_enc_col is None:
+        hashing_enc_col = FEATURE_FOR_HASHING_ENCODER
 
     item_features.set_index("item_id", inplace=True)
-    cols = item_features.columns
-    item_id = item_features.index
 
-    frequency_encoder = make_pipeline(
-        ce.count.CountEncoder(cols=cols, normalize=True), MinMaxScaler()
+    logging.info("Encoding feature 'brand' ...")
+
+    df1 = pd.DataFrame()
+    df1["brand"] = item_features["brand"].map({"Private": 0, "National": 1})
+
+    logging.info("Encoding item features with CountEncoder ...")
+
+    count_encoder = ce.CountEncoder(
+        cols=count_cols,
+        handle_unknown=-1,
+        handle_missing=-2,
+        min_group_size=5,
+        combine_min_nan_groups=True,
+        min_group_name="others",
+        normalize=True,
     )
-    X = frequency_encoder.fit_transform(item_features)
-    item_features_frequency_encoded = pd.DataFrame(
-        X, index=item_id, columns=cols
-    ).reset_index()
+    df2 = count_encoder.fit_transform(item_features[count_cols])
+    df2.columns = [i + "_count" for i in df2.columns]
 
-    item_features.reset_index(inplace=True)
+    logging.info("Encoding features with HashingEncoder ...")
 
-    return item_features_frequency_encoded
+    hashing_encoder = ce.HashingEncoder(cols=[hashing_enc_col])
+    df3 = hashing_encoder.fit_transform(item_features[[hashing_enc_col]])
+    df3.columns = [hashing_enc_col + "_" + str(i) for i in range(df3.shape[1])]
+
+    logging.info("Encoding item descriptions...")
+
+    df4 = encode_item_descriptions(item_features)
+
+    item_features_transformed = pd.concat([df1, df2, df3, df4], axis=1)
+
+    return item_features_transformed
 
 
-def item_features_descriptions_encoder(
+def encode_item_descriptions(
     item_features: pd.DataFrame,
     add_stop_words=None,
     concat_list=None,
 ) -> pd.DataFrame:
     """
-    Encodes commodity and sub-commodity descriptions with TF-IDF vectorizer.
+    Encodes commodity and sub-commodity descriptions with TF-IDF vectorizer and applies
+    HashingEncoder to change the number od columns from 300 to 32.
     """
-
-    logging.info("Encoding item features descriptions...")
 
     if concat_list is None:
         concat_list = CONCAT_LIST
-    item_features.loc[:, "item_desc"] = item_features.loc[:, concat_list].apply(
-        lambda x: " ".join(x).replace("/", " ").replace("-", " "), axis=1
-    )
     if add_stop_words is None:
         add_stop_words = ADD_STOP_WORDS
-    my_stop_words = text.ENGLISH_STOP_WORDS.union(add_stop_words)
 
+    my_stop_words = text.ENGLISH_STOP_WORDS.union(add_stop_words)
     vectorizer = TfidfVectorizer(
         ngram_range=(1, 2),
         analyzer="word",
         lowercase=True,
-        max_features=500,
+        max_features=300,
         stop_words=my_stop_words,
     )
-    X = vectorizer.fit_transform(item_features["item_desc"])
-    item_desc_vectorized = pd.DataFrame(
-        X.toarray(), index=item_features["item_id"]
-    ).reset_index()
+    df = pd.DataFrame()
+    df["item_desc"] = item_features.loc[:, concat_list].apply(
+        lambda x: " ".join(x).replace("/", " ").replace("-", " "), axis=1
+    )
+    X = vectorizer.fit_transform(df["item_desc"])
+    df_desc = X.toarray()
+    df_desc = pd.DataFrame(df_desc, index=item_features.index)
 
-    return item_desc_vectorized
+    hashing_encoder = ce.HashingEncoder(cols=df_desc.columns, n_components=32)
+    item_desc_encoded = hashing_encoder.fit_transform(df_desc)
+    item_desc_encoded.columns = [
+        "item_desc" + "_" + str(i) for i in range(item_desc_encoded.shape[1])
+    ]
 
-
-def fit_transform_item_features(
-    item_features: pd.DataFrame, vectorize_desc=False
-) -> pd.DataFrame:
-    """
-    Combines frequency encoding and item descriptions encoding
-    for training of the models.
-    """
-    logging.info("Transforming item_features for train dataset...")
-
-    df1 = item_features_frequency_encoder(item_features)
-
-    if vectorize_desc:
-        df2 = item_features_descriptions_encoder(item_features)
-        item_features_transformed = pd.merge(df1, df2, on="item_id", how="left")
-    else:
-        item_features_transformed = df1
-
-    return item_features_transformed
+    return item_desc_encoded
